45e1fb2d5dd564be358756b2c87ffa44db7b583c
Krishna Mahato <krishhtrishh9304@gmail.com>
2023-09-05 10:56:19 +0530

    ADD: upload page static design

--- src/pages/upload/upload.tsx
+++ src/pages/upload/upload.tsx
@@ -5,10 +5,17 @@ interface UploadProps {}
 const Upload: React.FC<UploadProps> = () => {
   return (
     <div className="upload-container">
-      <div>
+      <div className="upload-formlabel">Upload Form</div>
+      <div className="upload-form">
         <input type="text" placeholder="Enter your email" />
         <input type="text" placeholder="Enter your filename" />
       </div>
+
+      <div className="file-input-container">
+        <input type="file" />
+      </div>
+
+      <button>Upload</button>
     </div>
   );
 };

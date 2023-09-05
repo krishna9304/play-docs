3a7b8d981b369db6a9e868ee042f7952197c3c69
Krishna Mahato <krishhtrishh9304@gmail.com>
2023-09-05 13:33:04 +0530

    DONE✅: Upload file from UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import Upload from "./pages/upload/upload";
 import SearchNDownload from "./pages/searchAndDownload/searchAndDownload";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/search-and-download" element={<SearchNDownload />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/search-and-download" element={<SearchNDownload />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
 
--- src/pages/upload/upload.tsx
+++ src/pages/upload/upload.tsx
@@ -1,21 +1,79 @@
+import { ChangeEvent, useState } from "react";
 import "./upload.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 interface UploadProps {}
 
+const initialUploadForm = {
+  email: "",
+  filename: "",
+  user_file: "",
+};
 const Upload: React.FC<UploadProps> = () => {
+  const [uploadForm, setUploadForm] = useState<{
+    email: string;
+    filename: string;
+    user_file: any;
+  }>(initialUploadForm);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUploadForm((uf) => ({ ...uf, [e.target.name]: e.target.value }));
+  };
+
+  const handleUpload = async () => {
+    const requestFormData = new FormData();
+    requestFormData.append("email", uploadForm.email);
+    requestFormData.append("filename", uploadForm.filename);
+    requestFormData.append("user_file", uploadForm.user_file);
+
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/upload`,
+        requestFormData
+      );
+      toast(res.data.message);
+      setUploadForm(initialUploadForm);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="upload-container">
       <div className="upload-formlabel">Upload Form</div>
       <div className="upload-form">
-        <input type="text" placeholder="Enter your email" />
-        <input type="text" placeholder="Enter your filename" />
+        <input
+          onChange={handleChange}
+          name="email"
+          type="email"
+          value={uploadForm.email}
+          placeholder="Enter your email"
+        />
+        <input
+          onChange={handleChange}
+          name="filename"
+          type="text"
+          value={uploadForm.filename}
+          placeholder="Enter your filename"
+        />
       </div>
 
       <div className="file-input-container">
-        <input type="file" />
+        <input
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setUploadForm((uf) => ({
+              ...uf,
+              user_file: e.target.files ? e.target.files[0] : "",
+            }));
+          }}
+          type="file"
+        />
       </div>
 
-      <button>Upload</button>
+      <button value={uploadForm.user_file} onClick={handleUpload}>
+        Upload
+      </button>
     </div>
   );
 };
